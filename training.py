2e98cba0e5add199b5cdb6d079f6219dfc24a092
Lorenzo Olmo Marchal <114376800+lolmomarchal@users.noreply.github.com>
2025-04-06 20:35:50 -0700

    Update training.py

--- training.py
+++ training.py
@@ -98,6 +98,11 @@ class Trainer:
         print(f"Initializing {phase} directories")
         for batch in tqdm(dataset, total = len(dataset)):
             bags, positional, labels, x, y, tile_paths, scales, original_size, patient_id = batch.clone()
+            x = x.clone()
+            y = y.clone()
+            tile_paths = tile_paths.clone()
+            scales = scales.clone()
+            original_size = original_size.clone()
             patient_dir = os.path.join(phase_path, patient_id[0])
             patient_file = os.path.join(patient_dir, f"{patient_id[0]}.csv")
             os.makedirs(patient_dir, exist_ok=True)
